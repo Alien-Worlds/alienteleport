690989b17ffde267686caad5e8a416269e488691
Michael Yeates <michaeljyeates@gmail.com>
2020-08-30 01:55:55 +0100

    Add symbol to example config

--- oracle/config-example.js
+++ oracle/config-example.js
@@ -1,5 +1,6 @@
 module.exports = {
     precision: 4,
+    symbol: 'TLM',
     eos: {
         wsEndpoint: 'ws://178.63.44.179:8082',
         chainId: "f16b1833c747c43682f4386fca9cbb327929334a762755ebec17f6f23c9b8a12",

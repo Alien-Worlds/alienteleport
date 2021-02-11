4a4a5fae9b2b448f5e1ad2a33a9955d0ae422f97
Michael Yeates <michaeljyeates@gmail.com>
2021-02-11 14:25:10 +0000

    Use SameMath add function

--- contracts/teleporteth/Teleport.sol
+++ contracts/teleporteth/Teleport.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.6.12;
 pragma experimental "ABIEncoderV2";
 
 
-library Verify { 
+library Verify {
 
   function recoverSigner(bytes32 message, bytes memory sig)
        public
@@ -17,7 +17,7 @@ library Verify {
        bytes32 s;
 
        (v, r, s) = splitSignature(sig);
-       
+
        if (v != 27 && v != 28) {
            return (address(0));
        } else {
@@ -32,7 +32,7 @@ library Verify {
        returns (uint8, bytes32, bytes32)
    {
        require(sig.length == 65);
-       
+
        bytes32 r;
        bytes32 s;
        uint8 v;
@@ -45,8 +45,8 @@ library Verify {
            // final byte (first byte of the next 32 bytes)
            v := byte(0, mload(add(sig, 96)))
        }
-       
-       if (v < 27) 
+
+       if (v < 27)
            v += 27;
 
        return (v, r, s);
@@ -58,36 +58,36 @@ library Endian {
     /* https://ethereum.stackexchange.com/questions/83626/how-to-reverse-byte-order-in-uint256-or-bytes32 */
     function reverse64(uint64 input) internal pure returns (uint64 v) {
         v = input;
-    
+
         // swap bytes
         v = ((v & 0xFF00FF00FF00FF00) >> 8) |
             ((v & 0x00FF00FF00FF00FF) << 8);
-    
+
         // swap 2-byte long pairs
         v = ((v & 0xFFFF0000FFFF0000) >> 16) |
             ((v & 0x0000FFFF0000FFFF) << 16);
-    
+
         // swap 4-byte long pairs
         v = (v >> 32) | (v << 32);
     }
     function reverse32(uint32 input) internal pure returns (uint32 v) {
         v = input;
-    
+
         // swap bytes
         v = ((v & 0xFF00FF00) >> 8) |
             ((v & 0x00FF00FF) << 8);
-    
+
         // swap 2-byte long pairs
         v = (v >> 16) | (v << 16);
     }
     function reverse16(uint16 input) internal pure returns (uint16 v) {
         v = input;
-    
+
         // swap bytes
         v = (v >> 8) | (v << 8);
     }
 }
-    
+
 // ----------------------------------------------------------------------------
 // Safe maths
 // ----------------------------------------------------------------------------
@@ -180,13 +180,13 @@ contract Oracled is Owned {
 
     function regOracle(address _newOracle) public onlyOwner {
         require(!oracles[_newOracle], "Oracle is already registered");
-        
+
         oracles[_newOracle] = true;
     }
 
     function unregOracle(address _remOracle) public onlyOwner {
         require(oracles[_remOracle] == true, "Oracle is not registered");
-        
+
         delete oracles[_remOracle];
     }
 }
@@ -207,7 +207,7 @@ contract TeleportToken is ERC20Interface, Owned, Oracled {
 
     mapping(address => uint) balances;
     mapping(address => mapping(address => uint)) allowed;
-    
+
     mapping(uint64 => mapping(address => bool)) signed;
     mapping(uint64 => bool) public claimed;
 
@@ -223,7 +223,7 @@ contract TeleportToken is ERC20Interface, Owned, Oracled {
         uint8 chainId;
         address toAddress;
     }
-    
+
     // ------------------------------------------------------------------------
     // Constructor
     // ------------------------------------------------------------------------
@@ -346,7 +346,7 @@ contract TeleportToken is ERC20Interface, Owned, Oracled {
 
     function verifySigData(bytes memory sigData) private returns (TeleportData memory) {
         TeleportData memory td;
-        
+
         uint64 id;
         uint32 ts;
         uint64 fromAddr;
@@ -354,7 +354,7 @@ contract TeleportToken is ERC20Interface, Owned, Oracled {
         uint64 symbolRaw;
         uint8 chainId;
         address toAddress;
-        
+
         assembly {
             id := mload(add(add(sigData, 0x8), 0))
             ts := mload(add(add(sigData, 0x4), 8))
@@ -364,7 +364,7 @@ contract TeleportToken is ERC20Interface, Owned, Oracled {
             chainId := mload(add(add(sigData, 0x1), 36))
             toAddress := mload(add(add(sigData, 0x14), 37))
         }
-    
+
         td.id = Endian.reverse64(id);
         td.ts = Endian.reverse32(ts);
         td.fromAddr = Endian.reverse64(fromAddr);
@@ -372,56 +372,56 @@ contract TeleportToken is ERC20Interface, Owned, Oracled {
         td.symbolRaw = Endian.reverse64(symbolRaw);
         td.chainId = chainId;
         td.toAddress = toAddress;
-        
+
         require(thisChainId == td.chainId, "Invalid Chain ID");
-        require(block.timestamp < (td.ts + (60 * 60 * 24 * 30)), "Teleport has expired");
-        
+        require(block.timestamp < SafeMath.add(td.ts, (60 * 60 * 24 * 30)), "Teleport has expired");
+
         require(!claimed[td.id], "Already Claimed");
-        
+
         claimed[td.id] = true;
-        
+
         return td;
     }
 
     function claim(bytes memory sigData, bytes[] calldata signatures) public returns (address toAddress) {
         TeleportData memory td = verifySigData(sigData);
-        
+
         // verify signatures
         require(sigData.length == 69, "Signature data is the wrong size");
         require(signatures.length <= 10, "Maximum of 10 signatures can be provided");
-        
+
         bytes32 message = keccak256(sigData);
-        
+
         uint8 numberSigs = 0;
-        
+
         for (uint8 i = 0; i < signatures.length; i++){
             address potential = Verify.recoverSigner(message, signatures[i]);
-            
+
             // Check that they are an oracle and they haven't signed twice
             if (oracles[potential] && !signed[td.id][potential]){
                 signed[td.id][potential] = true;
                 numberSigs++;
-                
+
                 if (numberSigs >= 10){
                     break;
                 }
             }
         }
-        
+
         require(numberSigs >= threshold, "Not enough valid signatures provided");
 
         balances[address(0)] = balances[address(0)].sub(td.quantity);
         balances[td.toAddress] = balances[td.toAddress].add(td.quantity);
-        
+
         emit Claimed(td.id, td.toAddress, td.quantity);
-        
+
         return td.toAddress;
     }
 
     function updateThreshold(uint8 newThreshold) public onlyOwner returns (bool success) {
         if (newThreshold > 0){
             require(newThreshold <= 10, "Threshold has maximum of 10");
-            
+
             threshold = newThreshold;
 
             return true;

1a1f868426258525ab15347a3b2d03dfb0f5269a
Michael Yeates <michaeljyeates@gmail.com>
2021-02-20 22:24:13 +0000

    update example to use mainnet address

--- oracle/config-example.js
+++ oracle/config-example.js
@@ -5,7 +5,7 @@ module.exports = {
         wsEndpoint: 'ws://178.63.44.179:8082',
         chainId: "f16b1833c747c43682f4386fca9cbb327929334a762755ebec17f6f23c9b8a12",
         endpoint: 'https://testnet.waxsweden.org',
-        teleportContract: 'other.world',
+        teleportContract: 'other.worlds',
         oracleAccount: '',
         privateKey: '5C234dc...'
     },

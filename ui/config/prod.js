701efb31defa085085f918052ad81c10b4e1d92f
Michael Yeates <michaeljyeates@gmail.com>
2021-05-03 07:06:35 +0100

    Update api url

--- ui/config/prod.js
+++ ui/config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    waxEndpoint: 'https://wax.eosdac.io',
+    waxEndpoint: 'https://api.waxsweden.org',
     waxChainId: '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4',
     tlmContract: 'alien.worlds',
     teleportContract: 'other.worlds',
